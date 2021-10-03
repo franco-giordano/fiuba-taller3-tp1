66d87ef1307d666a6cfd7e93d7cb7d93888c5caa
franco-giordano <francogior98@gmail.com>
2021-10-03 16:49:25 -0300

    add inc-counter fetch and uncomment stuff

--- cloud-funcs/http-api/main.py
+++ cloud-funcs/http-api/main.py
@@ -13,26 +13,27 @@ PROJECT_ID = os.getenv('GOOGLE_CLOUD_PROJECT')
 TOPIC_NAME = "NEW_VISIT_EVENT"
 SHARDS_AMOUNT = 20
 
-# def init_counter(request):
-#     """Responds to any HTTP request.
-#     Args:
-#         request (flask.Request): HTTP request object.
-#     Returns:
-#         The response text or any set of values that can be turned into a
-#         Response object using
-#         `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
-#     """
+def init_counter(request):
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+    """
 
-#     if request.args and 'type' in request.args:
-#         # Add a new document
-#         db = firestore.Client() # doc_ref
-#         counter = Counter(SHARDS_AMOUNT, request.args.get('type') + "_shards")
-#         counter.init_counter(db)
-#         # logger.info("Counter home created!")
+    request_json = request.get_json()
+    if request_json and 'visit_type' in request_json:
+        # Add a new document
+        db = firestore.Client() # doc_ref
+        counter = Counter(SHARDS_AMOUNT, request_json['visit_type'] + "_shards")
+        counter.init_counter(db)
+        # logger.info("Counter home created!")
 
-#         return f'Created shards for {request.args.get("type")}'
-#     else:
-#         return 'Provide type with ?type=...'
+        return f"Created shards for {request_json['visit_type']}"
+    else:
+        return 'Provide type with ?visit_type=...'
 
 
 def get_counter(request):
--- static-web/assets/js/main.js
+++ static-web/assets/js/main.js
@@ -1,10 +1,16 @@
 (function ($) {
 
-	console.log();
 	let paths = window.location.pathname.split('/');
-	let type = paths[paths.length-1].slice(0,-5);
+	let type = paths[paths.length - 1].slice(0, -5);
 
 	// Contador de visitas
+	fetch(`https://southamerica-east1-taller3-fgiordano.cloudfunctions.net/inc-counter?visit_type=${type}`)
+		// .then(response => response.text())
+		// .then(data => { $('#contador').text('Contador de visitas: ' + data); })
+		// .catch(function (error) {
+		// 	console.log('Looks like there was a problem: \n', error);
+		// });
+
 	fetch(`https://southamerica-east1-taller3-fgiordano.cloudfunctions.net/get-counter?visit_type=${type}`)
 		.then(response => response.text())
 		.then(data => { $('#contador').text('Contador de visitas: ' + data); })
